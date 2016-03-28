3ca9d806b25452ffbfee7c2d6e44f9c78253cc59
Ryan Boye <rpboye@gmail.com>
2016-03-27 17:51:04 -0700

    Logging all players inventory to the console

--- control.lua
+++ control.lua
@@ -7,28 +7,15 @@ script.on_init(function()
     -- write_file('resources.txt', 'hello world')
 end)
 
-
--- Print contents of `tbl`, with indentation.
--- `indent` sets the initial level of indentation.
-function tprint (tbl, indent)
-  if not indent then indent = 0 end
-  for k, v in pairs(tbl) do
-    formatting = string.rep("  ", indent) .. k .. ": "
-    if type(v) == "table" then
-      print(formatting)
-      tprint(v, indent+1)
-    else
-      print(formatting .. v)
-    end
-  end
-end
-
 script.on_event(defines.events.on_tick, function(event)
     i = i + 1
-    -- game.player.print(i)
+    inv = ''
     if i % 200 == 0 then
-        for k,v in
-            pairs(game.player.get_inventory(1).get_contents()) do game.player.print(k .. v) 
+        for a,b in pairs(game.players) do
+            for k,v in pairs(game.players[a].get_inventory(1).get_contents()) do 
+                inv = inv .. (k .. ' ' .. v .. '\n') 
+            end
         end
+        game.write_file('test', inv)
     end
 end)
